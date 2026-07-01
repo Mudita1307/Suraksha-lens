0661de8befcb3bb5d15f8d5441854336e0d6a86c
Mudita Sharma <154345270+Mudita1307@users.noreply.github.com>
2026-07-01 15:34:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def login():
         "consultant.india@nhrf.no":"nhrf@sl2026",
         "reidun.ryland@nhrf.no":"nhrf@sl2026",
         "consultant.srilanka@nhrf.no":"nhrf@sl2026",
-        "ipenprogram@gmail.com" : "nhrf@sl2026"
+        "ipenprogram@gmail.com" : "nhrf@sl2026",
+        "munnadeblr@gmail.com":"mn@sl2026"
     }
 
     if submit:
